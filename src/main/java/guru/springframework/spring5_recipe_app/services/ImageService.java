80db8e05976ac1be45300a5bce6bf6de463118bb
magda <magda.vranau@gmail.com>
2025-01-22 14:40:48 +0200

    uploading images with spring mvc closes video 209

---
+++ src/main/java/guru/springframework/spring5_recipe_app/services/ImageService.java
@@ -0,0 +1,11 @@
+package guru.springframework.spring5_recipe_app.services;
+
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * Created by jt on 7/3/17.
+ */
+public interface ImageService {
+
+    void saveImageFile(Long recipeId, MultipartFile file);
+}
