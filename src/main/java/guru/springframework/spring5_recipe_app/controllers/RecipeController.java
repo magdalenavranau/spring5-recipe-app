d224053db43841ce0a3a71d4fc1e2eace3e9df79
magda <magda.vranau@gmail.com>
2025-01-25 21:25:22 +0200

    added an errorPage

--- src/main/java/guru/springframework/spring5_recipe_app/controllers/RecipeController.java
+++ src/main/java/guru/springframework/spring5_recipe_app/controllers/RecipeController.java
@@ -1,11 +1,14 @@
 package guru.springframework.spring5_recipe_app.controllers;
 
 import guru.springframework.spring5_recipe_app.commands.RecipeCommand;
+import guru.springframework.spring5_recipe_app.exceptions.NotFoundException;
 import guru.springframework.spring5_recipe_app.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by jt on 6/19/17.
@@ -55,5 +58,20 @@ public class RecipeController {
 
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(Exception exception){
+
+        log.error("Handling not found exception");
+        log.error(exception.getMessage());
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("404error");
+        modelAndView.addObject("exception", exception);
+
+        return modelAndView;
     }
 }
